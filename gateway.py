fc5ddcb109ef1d2b5d5c41b6887308fe8235b50d
Constantin Cazacu <71124836+constantin-cazacu@users.noreply.github.com>
2024-04-23 19:54:15 +0300

    Update gateway.py

--- gateway.py
+++ gateway.py
@@ -3,84 +3,149 @@ import requests
 import base64
 from pydantic import BaseModel
 import time
+
 app = FastAPI()
 
+import os
+from dotenv import load_dotenv
 
-REGISTER_SERVICE_URL = 'http://127.0.0.1:8050/'
-PHOTO_SERVICE_URL = 'http://127.0.0.1:8051/photo'
-VIDEO_SERVICE_URL = 'http://127.0.0.1:8052/video'
+# Load environment variables from .env file
+load_dotenv()
+
+# Retrieve URLs from environment variables
+REGISTER_SERVICE_URL = os.environ.get('REGISTER_SERVICE_URL')
+
 
 class ServiceRegister(BaseModel):
     service_name: str
+
+
+class Message(BaseModel):
+    sender_id: int
+    recipient_id: int
+    content: str
+
+
 class VideoUpload(BaseModel):
     video: str
     description: str
     publish_date: int
 
+
 class PhotoUpload(BaseModel):
     image: str
     description: str
     publish_date: int
-    
+
+
+@app.post("/health")
+async def urmom():
+    return "sss"
+
 
 @app.post("/register")
 async def register_service(data: ServiceRegister):
     print(data.service_name)
     payload = {
-        'service_name': data.service_name, 
-        'start_date': int(time.time())
+        'service_name': data.service_name
     }
-    
+
     # headers = {'Content-Type': 'application/json'}
-    response = requests.post(f"{REGISTER_SERVICE_URL}register_service", json=payload)
+    response = requests.post(f"{REGISTER_SERVICE_URL}/register_service/{data.service_name}")
     if response.status_code == 200:
-            return {"message": "Service registered successfully"}
+        return {"message": "Service registered successfully"}
     else:
-            raise HTTPException(status_code=500, detail="Failed to upload video")
- 
+        raise HTTPException(status_code=500, detail="Failed to upload video")
+
     # return "urmom"
+
 
 @app.post("/upload-video")
 async def upload_video(data: VideoUpload):
-    try: 
-        video_bytes = base64.b64decode(data.video.encode('utf-8'))
+    response = requests.get(f"{REGISTER_SERVICE_URL}/get_service/video_service")
+    response_payload = response.json()
+    print(response_payload)
+    if response_payload["is_active"]:
+        # print("service is registered")
+        try:
+            video_bytes = base64.b64decode(data.video.encode('utf-8'))
 
-        payload = {
-            'video': data.video,
-            'description': data.description,
-            'publish_date': data.publish_date
-        }
+            payload = {
+                'video': data.video,
+                'description': data.description,
+                'publish_date': data.publish_date,
+                'uid': "data.uid"
+            }
 
-        response = requests.post(VIDEO_SERVICE_URL, json=payload)
+            response = requests.post(response_payload["address"], json=payload)
 
-        if response.status_code == 200:
-            return {"message": "Video uploaded successfully"}
-        else:
-            raise HTTPException(status_code=500, detail="Failed to upload video")
+            if response.status_code == 200:
+                return {"message": "Video uploaded successfully"}
+            else:
+                raise HTTPException(status_code=500, detail="Failed to upload video")
 
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=str(e))
+    else:
+        raise HTTPException(status_code=404, detail="Service not found")
+
+
+@app.post("/send-message/")
+async def send_message(data: Message):
+    response = requests.get(f"{REGISTER_SERVICE_URL}/get_service/message_service")
+    response_payload = response.json()
+    print(response_payload)
+    if response_payload["is_active"]:
+        # print("service is registered")
+        try:
+            payload = {
+                'sender_id': data.sender_id,
+                'recipient_id': data.recipient_id,
+                'content': data.content,
+            }
+
+            response = requests.post(f'{response_payload["address"]}/send-message/', json=payload)
+
+            if response.status_code == 200:
+                return {"message": "Message sent successfully"}
+            else:
+                raise HTTPException(status_code=500, detail="Failed to send message")
+
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=str(e))
+    else:
+        raise HTTPException(status_code=404, detail="Service not found")
 
 @app.post("/upload-photo/")
 async def upload_photo(data: PhotoUpload):
-    try: 
-        image_bytes = base64.b64decode(data.image.encode('utf-8'))
+    response = requests.get(f"{REGISTER_SERVICE_URL}/get_service/photo_service")
+    response_payload = response.json()
+    # print(response_payload)
+    if response_payload["is_active"]:
+        print("service is registered")
+        try:
+            image_bytes = base64.b64decode(data.image.encode('utf-8'))
 
-        payload = {
-            'image': data.image,
-            'description': data.description,
-            'publish_date': data.publish_date
-        }
+            payload = {
+                'image': data.image,
+                'description': data.description,
+                'publish_date': data.publish_date,
+                'uid': "data.uid"
 
-        response = requests.post(PHOTO_SERVICE_URL, json=payload)
+            }
+            # print("sm")
+            response = requests.post(response_payload["address"], json=payload)
+            # print("sm")
 
-        if response.status_code == 200:
-            return {"message": "Photo uploaded successfully"}
-        else:
-            raise HTTPException(status_code=500, detail="Failed to upload photo")
+            if response.status_code == 200:
+                return {"message": "Photo uploaded successfully"}
+            else:
+                raise HTTPException(status_code=500, detail="Failed to upload photo")
 
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=str(e))
+    else:
+        raise HTTPException(status_code=404, detail="Service not found")
 
 
 if __name__ == "__main__":
